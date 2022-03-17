c4c458777dbfeac6ae88c338a59624284e2bae5d
SandalFyr <101326916+SandalFyr@users.noreply.github.com>
2022-03-17 16:12:18 +0100

    Revert "Revert "Intruktions register, zero fill og  sign extender""
    This reverts commit ea3ad0ccbba5c89634fbf7257086392df4a6f6ac.

--- PWB/PWB.srcs/sources_1/new/Instruction Register.vhd
+++ PWB/PWB.srcs/sources_1/new/Instruction Register.vhd
@@ -1,11 +1,11 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company: DTU- digital desing
+-- Engineer: hold 1
 -- 
 -- Create Date: 10.03.2022 11:23:48
 -- Design Name: 
 -- Module Name: Instruction Register - Behavioral
--- Project Name: 
+-- Project Name: PWB
 -- Target Devices: 
 -- Tool Versions: 
 -- Description: 
@@ -42,7 +42,17 @@ IR: out std_logic_vector (15 downto 0)
 End InstructionRegister;
 Architecture IR_Behavorial of InstructionRegister is
 
-
 Begin
+
+InstructionReg: process(reset,clk)                    --InstructionReg 
+begin 
+    if Reset ='1' then					    -- Asynchrounes reset
+		IR <=x"0000";
+	elsif clk'event and clk='1' then
+		if IL = '1' then                 -- check for tilhørende load værdi
+			IR <= Instruction_In;                    -- Instruction_In indsættes på IR     
+	    end if;
+	end if;
+end process;
 
 End;
